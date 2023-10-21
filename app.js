6102634be294e265feefd80cd46275260ba95cc5
salman <alysalman15@gmail.com>
2023-10-21 15:43:20 +0500

    push

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 var form = document.getElementById("form")
 
-
-event.preventDefault();
-
 function login() {
-    var userName = document.getElementById("username").value
-    var password = document.getElementById("pass").value
-    if (userName === 'abc' && password === '123') {
-        alert("done")
-        window.location.assign("quiz.html")
+    var userName = document.getElementById("username").value;
+    var password = document.getElementById("pass").value;
+
+    if (userName === "" || password === "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Username and password cant be empty!',
+        });
+    }
+    else if (userName === 'abc' && password === '123') {
+        alert("Login Successfully!");
+        window.location.assign("quiz.html");
     } else {
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
-            text: 'Please enter Correct username and passowrd !',
-        })
+            text: 'Please enter correct username and password!',
+        });
     }
-
-    userName.value = "";
-    password.value = "";
+    document.getElementById("username").value = "";
+    document.getElementById("pass").value = "";
 
 }
-
