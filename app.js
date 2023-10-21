1053ac272f53af1e5fe2397e77448ac94b19ac38
salman <alysalman15@gmail.com>
2023-10-21 17:52:47 +0500

    added data

--- app.js
+++ app.js
@@ -1,3 +1,61 @@
+var quiz = [
+    {
+        question: "What is the capital city of Pakistan?",
+        option1: "Karachi",
+        option2: "Lahore",
+        option3: "Islamabad",
+        option4: "Gilgit",
+        answer: "Islamabad",
+    },
+    {
+        question: "What is the official name of the national airline of Pakistan?",
+        option1: "Pakistan Airways",
+        option2: "Pakistan International Airlines (PIA)",
+        option3: "Air Pakistan",
+        option4: "Pakistan Air Connect",
+        answer: "Pakistan International Airlines (PIA)",
+    },
+    {
+        question: "Which sport is Pakistan's national game?",
+        option1: "Polo",
+        option2: "Football",
+        option3: "Cricket",
+        option4: "Hockey",
+        answer: "Hockey",
+    },
+    {
+        question: "What is the highest mountain in Pakistan and the second-highest in the world?",
+        option1: "Mount Everest",
+        option2: "K2",
+        option3: "Nanga Parbat",
+        option4: "Mount Kilimanjaro",
+        answer: "K2",
+    },
+    {
+        question: "What is the largest province by area in Pakistan?",
+        option1: "Sindh",
+        option2: "Punjab",
+        option3: "Balochistan",
+        option4: "Kpk",
+        answer: "Balochistan",
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var form = document.getElementById("form")
 
 function login() {
