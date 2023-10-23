bc4952bc217bc6855a2176278ccbc7cec2e68707
ahmedraza <ahmed.raza1054@gmail.com>
2023-10-23 21:20:34 +0500

    code push

--- app.js
+++ app.js
@@ -8,118 +8,115 @@ var timer = document.querySelector(".timer");
 var quiz = document.getElementById("quiz");
 var index = 0;
 var score = 0;
-var timeLeft = 10;
 var counter;
-
+var timeValue = 5;
 
 var questions = [
-    {
-        question: "What is the capital city of Pakistan?",
-        option1: "Karachi",
-        option2: "Lahore",
-        option3: "Islamabad",
-        option4: "Gilgit",
-        answer: "Islamabad",
-    },
-    {
-        question: "What is the official name of the national airline of Pakistan?",
-        option1: "Pakistan Airways",
-        option2: "Pakistan International Airlines (PIA)",
-        option3: "Air Pakistan",
-        option4: "Pakistan Air Connect",
-        answer: "Pakistan International Airlines (PIA)",
-    },
-    {
-        question: "Which sport is Pakistan's national game?",
-        option1: "Polo",
-        option2: "Football",
-        option3: "Cricket",
-        option4: "Hockey",
-        answer: "Hockey",
-    },
-    {
-        question: "What is the highest mountain in Pakistan and the second-highest in the world?",
-        option1: "Mount Everest",
-        option2: "K2",
-        option3: "Nanga Parbat",
-        option4: "Mount Kilimanjaro",
-        answer: "K2",
-    },
-    {
-        question: "What is the largest province by area in Pakistan?",
-        option1: "Sindh",
-        option2: "Punjab",
-        option3: "Balochistan",
-        option4: "Kpk",
-        answer: "Balochistan",
-    }
-]
-
+  {
+    question: "What is the capital city of Pakistan?",
+    option1: "Karachi",
+    option2: "Lahore",
+    option3: "Islamabad",
+    option4: "Gilgit",
+    answer: "Islamabad",
+  },
+  {
+    question: "What is the official name of the national airline of Pakistan?",
+    option1: "Pakistan Airways",
+    option2: "Pakistan International Airlines (PIA)",
+    option3: "Air Pakistan",
+    option4: "Pakistan Air Connect",
+    answer: "Pakistan International Airlines (PIA)",
+  },
+  {
+    question: "Which sport is Pakistan's national game?",
+    option1: "Polo",
+    option2: "Football",
+    option3: "Cricket",
+    option4: "Hockey",
+    answer: "Hockey",
+  },
+  {
+    question:
+      "What is the highest mountain in Pakistan and the second-highest in the world?",
+    option1: "Mount Everest",
+    option2: "K2",
+    option3: "Nanga Parbat",
+    option4: "Mount Kilimanjaro",
+    answer: "K2",
+  },
+  {
+    question: "What is the largest province by area in Pakistan?",
+    option1: "Sindh",
+    option2: "Punjab",
+    option3: "Balochistan",
+    option4: "Kpk",
+    answer: "Balochistan",
+  },
+];
 
 function login() {
-    var userName = document.getElementById("username").value;
-    var password = document.getElementById("pass").value;
+  var userName = document.getElementById("username").value;
+  var password = document.getElementById("pass").value;
 
-    if (userName === "" || password === "") {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Username and password cant be empty!',
-        });
-    }
-    else if (userName === 'smit' && password === 'smit') {
-        alert("Login Successfully!");
-        window.location.assign("start.html");
-    } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Please enter correct username and password!',
-        });
-    }
-    document.getElementById("username").value = "";
-    document.getElementById("pass").value = "";
-
+  if (userName === "" || password === "") {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Username and password cant be empty!",
+    });
+  } else if (userName === "smit" && password === "smit") {
+    alert("Login Successfully!");
+    window.location.assign("start.html");
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please enter correct username and password!",
+    });
+  }
+  document.getElementById("username").value = "";
+  document.getElementById("pass").value = "";
 }
 
-
 function forget() {
-    Swal.fire("Username: smit" + " " + "\n" + "password: smit")
+  Swal.fire("Username: smit" + " " + "\n" + "password: smit");
 }
 
 function renderQuestion() {
-    var question = document.getElementById("question");
-    var options = document.getElementsByName("option");
-    clearInterval(counter)
-    startTimer(timeLeft)
-    for (var i = 0; i < options.length; i++) {
-        if (options[i].checked) {
-            if (options[i].value === questions[index - 1].answer) {
-                score++;
-            }
-        }
+  var question = document.getElementById("question");
+  var options = document.getElementsByName("option");
+  clearInterval(counter);
+  startTimer(timeValue);
+  for (var i = 0; i < options.length; i++) {
+    if (options[i].checked) {
+      if (options[i].value === questions[index - 1].answer) {
+        score++;
+      }
     }
+  }
 
-    // function quitBtn() {
-    //     window.location.reload();
-    // }
-    //  quitBtn()
+  // function quitBtn() {
+  //     window.location.reload();
+  // }
+  //  quitBtn()
 
-    if (!questions[index]) {
-        showScore.innerHTML = `
+  if (!questions[index]) {
+    clearInterval(counter);
+    showScore.innerHTML = `
           <div class="score" id="result">
             <h4>Your score is ${score} out of ${options.length}</h4>
             <button id="quit" onclick="window.location.assign('start.html')">Play again</button>
           </div>
         `;
-        question.style.display = "none"
-        scoreCart.style.display = "block"
-        return;
-    }
+    question.style.display = "none";
+    scoreCart.style.display = "block";
+    return;
+  }
 
-
-    question.innerHTML = `
+  question.innerHTML = `
     <div class="quiz" id="quiz">
+
         <div class="heading" id="heading">
           <h3>Quiz App</h3>
         </div>
@@ -142,31 +139,30 @@ function renderQuestion() {
           <button onclick="next()">Next</button>
         </div>
       </div>
-    `
+    `;
 }
 
 // renderQuestion()
 
 function next() {
-    index++;
-    renderQuestion()
+  index++;
+  renderQuestion();
 }
-
 
 function strtquizBtn() {
-    document.documentElement.requestFullscreen();
+  document.documentElement.requestFullscreen();
 }
 
-
-function startTimer() {
-    counter = setInterval(function () {
-        timer.textContent = timeLeft;
-        timeLeft--;
-        if (timeLeft === 0) {
-            clearInterval(counter);
-            renderQuestion();
-        }
-    }, 1000);
+function startTimer(time) {
+  counter = setInterval(timer, 1000);
+  function timer() {
+    timer.innerHTML = time;
+    time--;
+    if (time < 0) {
+      clearInterval(counter);
+      renderQuestion();
+    }
+  }
 }
 
-renderQuestion()
+renderQuestion();
